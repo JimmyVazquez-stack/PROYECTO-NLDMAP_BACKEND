aeb1f520557a451bb3ea904ae2a0a5adad79f90d
Eduardo Vasquez <calle13.201@gmail.com>
2026-08-07 14:31:58 -0600

    implementar AuthController con endpoint de autenticación externa

--- NLDMAP.API/Controllers/AuthController.cs
+++ NLDMAP.API/Controllers/AuthController.cs
@@ -1,6 +1,36 @@
-﻿namespace NLDMAP.API.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using NLDMAP.Application.DTOs.Auth;
+using NLDMAP.Application.UseCases.Auth;
 
-public class AuthController
+
+namespace NLDMAP.API.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthenticateExternalUserUseCase _authenticateUseCase;
+
+        public AuthController(AuthenticateExternalUserUseCase authenticateUseCase)
+        {
+            _authenticateUseCase = authenticateUseCase;
+        }
     
+        [HttpPost("external-login")]
+        public async Task<IActionResult> ExternalLogin([FromBody] ExternalAuthDto request)
+        {
+            try
+            {   
+                //Validar caso de uso y registrar al ciudadano si es nuevo
+                var userEmail = await _authenticateUseCase.ExecuteAsync(request);
+                //Implementar logica de signin de openiddict
+
+                return Ok(new { Message = " Autenticación exitosa", Token = "JWT_AQUI" });
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                return Unauthorized(new { Error = ex.Message });
+            }
+        }
+    }   
 }
