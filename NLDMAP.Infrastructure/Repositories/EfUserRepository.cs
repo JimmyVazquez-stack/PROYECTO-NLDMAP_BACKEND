a284e4bd325822601c6ea8fd96efa7eb14ce45a1
Eduardo Vasquez <calle13.201@gmail.com>
2026-08-07 14:33:36 -0600

    refactor: renombrar EfUsuarioRepository a EfUserRepository y traducir entidad a User

---
+++ NLDMAP.Infrastructure/Repositories/EfUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using NLDMAP.Domain.Interfaces;
+using NLDMAP.Domain.Entities;
+
+namespace NLDMAP.Infrastructure.Repositories
+{
+    public class EfUserRepository : IUserRepository
+    {
+        //Inyectar despues aqui el Nldmaodbcontext
+        public async Task<User?> GetByEmailAsync(string email)
+        {
+           //falta implementar busqueda real en postgresql
+            return await Task.FromResult<User?>(null);
+        }
+        
+        public async Task AddSync(User user)
+        {
+            //implementar insercion real en postgresql    
+            await Task.CompletedTask;
+        }
+    }
+}
