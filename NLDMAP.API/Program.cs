e9283415ea92bf1d2092dbf6bffbc8b45664f05d
Eduardo Vasquez <calle13.201@gmail.com>
2026-08-07 14:36:01 -0600

    configurar inyección de dependencias para capas de aplicación e infraestructura

--- NLDMAP.API/Program.cs
+++ NLDMAP.API/Program.cs
@@ -1,9 +1,13 @@
+using NLDMAP.Application;
+using NLDMAP.Infrastructure;
 using NLDMAP.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // 1. Inyección de la infraestructura (Base de datos)
 builder.Services.AddPersistenceInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
 
 var app = builder.Build();
 
