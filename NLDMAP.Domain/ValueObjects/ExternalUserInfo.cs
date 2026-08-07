6bad7d4d2d830ebd9afc3f68461da84dbc83a64d
Eduardo Vasquez <calle13.201@gmail.com>
2026-08-07 14:33:54 -0600

    ajustar declaración de namespace en ExternalUserInfo para consistencia de estilo

--- NLDMAP.Domain/ValueObjects/ExternalUserInfo.cs
+++ NLDMAP.Domain/ValueObjects/ExternalUserInfo.cs
@@ -1,9 +1,9 @@
-﻿namespace NLDMAP.Domain.ValueObjects;
+﻿namespace NLDMAP.Domain.ValueObjects
 {
     public class ExternalUserInfo
     {
         public string Email { get; set; } = string.Empty;
         public string FullName { get; set; } = string.Empty;
-        
+
     }
 }
