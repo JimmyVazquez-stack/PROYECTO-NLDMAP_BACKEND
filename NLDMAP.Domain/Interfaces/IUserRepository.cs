008800449bdee584e5dba4a39dd4c3b8f4262057
Eduardo Vasquez <calle13.201@gmail.com>
2026-08-07 14:34:29 -0600

    definir contrato IUserRepository en el dominio para la gestión de usuarios

---
+++ NLDMAP.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using NLDMAP.Domain.Entities;
+
+namespace NLDMAP.Domain.Interfaces;
+
+    public interface IUserRepository
+    {
+        //busca un usuario por email
+        Task<User?> GetByEmailAsync(string email);
+
+        //agrega un nuevo ciudadano
+        Task AddSync(User user);
+    }
