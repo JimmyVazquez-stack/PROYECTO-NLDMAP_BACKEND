8749481be5ae0e34630d3cd4e5c8b99f51238176
Eduardo Vasquez <calle13.201@gmail.com>
2026-08-10 14:39:18 -0600

    implementar DbContextFactory para migraciones en tiempo de diseño

---
+++ NLDMAP.Infrastructure/Persistence/NldmapDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace NLDMAP.Infrastructure.Persistence
+{
+//esta clase solo es detected y utilizada por el comando 'dotnet ef'
+    public class NldmapDbContextFactory : IDesignTimeDbContextFactory<NldmapDbContext>
+    {
+        public NldmapDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<NldmapDbContext>();
+
+            var connectionString = "Host=localhost;Port=5433;Database=nldmap_db;Username=postgres;Password=admin1234";
+            
+            optionsBuilder.UseNpgsql(connectionString, x => x.UseNetTopologySuite());
+            
+            return new NldmapDbContext(optionsBuilder.Options);
+
+        }
+    }
+}  
