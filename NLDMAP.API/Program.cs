7a0566713cf8e4fd8e5817ba85ed7549d2dd9289
Eduardo Vasquez <calle13.201@gmail.com>
2026-08-11 09:13:55 -0600

    configurar servicios y pipeline para controladores y Swagger en API

--- NLDMAP.API/Program.cs
+++ NLDMAP.API/Program.cs
@@ -4,12 +4,35 @@ using NLDMAP.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//registro de servicios
+//habilitar controladores
+builder.Services.AddControllers();
+
+//habilitar swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 // 1. Inyección de la infraestructura (Base de datos)
 builder.Services.AddPersistenceInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure();
 
 var app = builder.Build();
+
+//Middlewares
+//mostrar swagger solo en entorno desarrollo
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+//mapear rutas de controladores
+app.MapControllers();
 
 // 2. Endpoint de prueba rápida
 app.MapGet("/", () => "¡La API está corriendo y la Infraestructura se inyectó correctamente!");
