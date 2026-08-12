f0072af8b4e6ce965ecacc13e4e096edd600e00a
Eduardo Vasquez <calle13.201@gmail.com>
2026-08-12 09:55:33 -0600

    agregar endpoint de prueba para verificar conexión a base de datos en AuthController

--- NLDMAP.API/Controllers/AuthController.cs
+++ NLDMAP.API/Controllers/AuthController.cs
@@ -5,22 +5,61 @@ using NLDMAP.Application.UseCases.Auth;
 
 namespace NLDMAP.API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
+    [ApiController] //validacion por parte de .NET los datos entrantes
+    [Route("api/[controller]")] //ruta base /api/Auth
     public class AuthController : ControllerBase
     {
-        private readonly AuthenticateExternalUserUseCase _authenticateUseCase;
+        //Casos de uso
+        //private readonly IUserRepository _userRepository
+        //private readonly AuthenticateExternalUserUseCase _authenticateUseCase;
 
-        public AuthController(AuthenticateExternalUserUseCase authenticateUseCase)
+        //Constructor
+        //public AuthController(AuthenticateExternalUserUseCase authenticateUseCase)
+        
+        public AuthController()
         {
-            _authenticateUseCase = authenticateUseCase;
+           // _authenticateUseCase = authenticateUseCase;
+           //_userRepository = userRepository
         }
     
-        [HttpPost("external-login")]
+        // Endpoint al que se intenta acceder
+        [HttpGet("test-db")]
+        public IActionResult TestDatabase()
+        {
+            try
+            {
+                //Aqui ira llamada real a _userRepository
+                //var user = await _userRepository.CrearOObtenerAsync(...);
+                
+                //Wrapper estandar para interfaz
+                var response = new
+                {
+                    succesfull = true,
+                    message = "POSTGRESQL : OPERACION EXITOSA",
+                    data = new
+                    {
+                        id = Guid.NewGuid(),
+                        role = "Ciudadano",
+                        responseTimeMs = 15
+                    }
+                };
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    succesfull = false,
+                    message = $"Error en modulo 1: {ex.Message}",
+                });
+            }
+        }
+
+        /* [HttpPost("external-login")]
         public async Task<IActionResult> ExternalLogin([FromBody] ExternalAuthDto request)
         {
             try
-            {   
+            {
                 //Validar caso de uso y registrar al ciudadano si es nuevo
                 var userEmail = await _authenticateUseCase.ExecuteAsync(request);
                 //Implementar logica de signin de openiddict
@@ -31,6 +70,7 @@ namespace NLDMAP.API.Controllers
             {
                 return Unauthorized(new { Error = ex.Message });
             }
-        }
+        }*/
+
     }   
 }
