bc3f47d932b569f7a97094cd673f549ab9f3db65
Eduardo Vasquez <calle13.201@gmail.com>
2026-08-12 12:22:45 -0600

    expandir entidad User con datos de perfil y autenticación SSO

--- NLDMAP.Domain/Entities/User.cs
+++ NLDMAP.Domain/Entities/User.cs
@@ -3,12 +3,24 @@
     public class User
     {
         public Guid Id { get; set; } = Guid.NewGuid();
-        public String FullName { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
+        //datos Oauth2
+        public required string FullName { get; set; }
+        public required string Email { get; set; }
+        public required string SsoProvider { get; set; }  //Google / Facebook 
+        public required string SsoProviderId { get; set; } //Id unico que otorga el proveedor
+        
+        
+        //perfil reportante
+        public string? Curp { get; set; }
+        public required string Sex { get; set; }
+        public required string Gender { get; set; }
+        public required string MaritalStatus { get; set; }
+        public string? Schooling { get; set; }
         public string PhoneNumber { get; set; } = string.Empty;
-        public string Role { get; set; } = string.Empty;
-        public string AlertRadioKm { get; set; } = string.Empty;
-        public string PasswordHash { get; set; } = string.Empty;
-
+        public string Role { get; set; } = "Ciudadano";
+        public int AlertRadioKm { get; set; } = 10;
+        
+        //control de anonimato
+        public bool BeAnonymous { get; set; }
     }
 }
