7c0d600bd50f4c3ab28c8b4a9465715636452a0f
Eduardo Vasquez <calle13.201@gmail.com>
2026-08-12 09:55:16 -0600

    configurar inyección de persistencia en infraestructura mediante IConfiguration

--- NLDMAP.Infrastructure/DependencyInjection.cs
+++ NLDMAP.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using NLDMAP.Domain.Interfaces;
 using NLDMAP.Infrastructure.ExternalServices;
+using NLDMAP.Infrastructure.Persistence;
 using NLDMAP.Infrastructure.Repositories;
 
 
@@ -8,8 +10,17 @@ namespace NLDMAP.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
+            //modulo 1 : Identidad y seguridad 
+            services.AddPersistenceInfrastructure(configuration);
+            
+            //modulo 2 : Documentos y caracteristicas 
+            // services.AddMongoDbPersistence(configuration);
+            
+            //Modulo 3: Grafos
+            //services.AddNeo4jPersistence(configuration);
+            
             services.AddScoped<IUserRepository, EfUserRepository>();
             services.AddScoped<IExternalAuthValidator, GoogleAuthVlidator>();
         
