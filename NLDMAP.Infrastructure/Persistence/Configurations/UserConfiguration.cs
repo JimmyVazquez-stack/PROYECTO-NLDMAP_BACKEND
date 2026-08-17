e9aca57d16bf8c3c0ac9fe852fd73431c2cc765c
Eduardo Vasquez <calle13.201@gmail.com>
2026-08-17 09:21:24 -0600

    configurar entidad User con email único y requerido

---
+++ NLDMAP.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLDMAP.Domain.Entities;
+
+
+namespace NLDMAP.Infrastructure.Persistence.Configurations
+{
+
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(u => u.Id);
+
+            //email unico
+            builder.HasIndex(e => e.Email).IsUnique();
+            
+            //optimizacion espacio en postgresql
+            builder.Property(u => u.Email)
+                .HasMaxLength(150)
+                .IsRequired();
+        }
+    }
+}
