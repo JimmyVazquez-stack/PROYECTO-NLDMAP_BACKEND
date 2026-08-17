b3f0a7ad53425c63f8a1f8ca13d8bc5c344bdf96
Eduardo Vasquez <calle13.201@gmail.com>
2026-08-17 09:20:26 -0600

    configurar la entidad MissingPerson para persistencia con EF Core

---
+++ NLDMAP.Infrastructure/Persistence/Configurations/MissingPersonConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLDMAP.Domain.Entities;
+
+namespace NLDMAP.Infrastructure.Persistence.Configurations;
+
+public class MissingPersonConfiguration : IEntityTypeConfiguration<MissingPerson>
+{
+    public void Configure(EntityTypeBuilder<MissingPerson> builder)
+    {
+        builder.HasKey(m => m.Id);
+        
+        //restricciones de longitud
+        builder.Property(m => m.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(m => m.LastName)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(m => m.Curp)
+            .HasMaxLength(18);
+
+        builder.Property(m => m.Nationality)
+            .HasMaxLength(50);
+
+    }
+}
