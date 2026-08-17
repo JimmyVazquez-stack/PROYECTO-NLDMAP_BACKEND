d14eb3996506c664f9dfff80fe9fa38dc8af0ad2
eduardo <eduardovasquez4545@hotmail.com>
2026-08-17 15:11:28 -0600

    refactor: reemplazar enum ReportStatus por string en entidad Report

--- NLDMAP.Domain/Entities/Report.cs
+++ NLDMAP.Domain/Entities/Report.cs
@@ -1,67 +1,30 @@
 using NLDMAP.Domain.ValueObjects;
 
-namespace NLDMAP.Domain.Entities;
-
-public class Report 
+namespace NLDMAP.Domain.Entities
 {
-    public  Guid Id { get; set; } = Guid.NewGuid();
-    
-    //llave que conecta al reporte con la persona en la BD
-    public Guid MissingPersonId { get; private set; }
-    
-    //Relacion con el ciudadano que reporta
-    public Guid UserId { get; private set; }
-    public string MissingPersonRelation { get; private set; } //Madre, amigo/conocido etc.
-    
-    //informacion de la desaparicion
-    public DissapearEvents Events { get; private set; }
-
-    //Datos de consentimiento legal
-    public bool ConsentUseExclusive { get; private set; }
-    public bool RequestInformationPublic { get; private set; }
-
-    //estado del reporte
-    public ReportStatus Status { get; private set; } = ReportStatus.Pendiente;
-    
-    //constructor vacio requerido por EF
-    protected Report() {}
-    
-    //constructor de dominio
-    public Report(
-        Guid missingPersonId,
-        Guid userId,
-        string missingPersonRelation,
-        DissapearEvents events,
-        bool consentUseExclusive,
-        bool requestInformationPublic)
+    public class Report
     {
-        if (missingPersonId ==  Guid.Empty)
-            throw  new ArgumentException("El ID de la persona no puede estar vacio.", nameof(missingPersonId));
-        
-        if (userId ==  Guid.Empty)
-            throw  new ArgumentException("El ID del usuario no puede estar vacio.", nameof(userId));
+        public Guid Id { get; set; } = Guid.NewGuid();
 
-        if (string.IsNullOrWhiteSpace(missingPersonRelation))
-            throw new ArgumentException("La realcion la persona es obligatoria", nameof(missingPersonRelation));
+        //Relacion con el ciudadano que reporta
+        public Guid MissingPersonId { get; private set; }
+        public Guid UserId { get; private set; }
+        public  string MissingPersonRelation { get; private set; } //Madre, amigo/conocido etc.
+
+        //informacion de la desaparicion
+        public  DisappearanceEvents Events { get; private set; }
+
+        //Datos de consentimiento legal
+        public bool ConsentUseExclusive { get; private set; }
+        public bool RequestInformationPublic { get; private set; }
+
+        //estado del reporte
+        public string ReportStatus { get; private set; } = "PENDIENTE"; // "SIN_VALIDAR", "ACTIVO"
         
-        MissingPersonId = missingPersonId;
-        UserId = userId;
-        MissingPersonRelation = missingPersonRelation;
-        ConsentUseExclusive = consentUseExclusive;
-        RequestInformationPublic = requestInformationPublic;
-        Events = events;
+        //Metodos para cambiar el estado 
+        public void MarkAsActive() => ReportStatus = "ACTIVO";
+        public void MarkAsInvalid() => ReportStatus = "SIN_VALIDAR";
 
     }
-
-    public void MaskAsActive() => Status = ReportStatus.Activo;
-    public void MaskAsInactive() => Status = ReportStatus.SinValidar;
-
+    
 }
-
-public enum ReportStatus
-{
-    Pendiente,
-    SinValidar,
-    Activo
-}
-
