a339ab58013ab1653e93aa312df7104e990478c3
Eduardo Vasquez <calle13.201@gmail.com>
2026-08-17 09:20:11 -0600

    refactorizar DissapearEvents como un Value Object en el dominio

--- NLDMAP.Domain/Entities/Report.cs
+++ NLDMAP.Domain/Entities/Report.cs
@@ -1,3 +1,5 @@
+using NLDMAP.Domain.ValueObjects;
+
 namespace NLDMAP.Domain.Entities;
 
 public class Report 
@@ -63,22 +65,3 @@ public enum ReportStatus
     Activo
 }
 
-public class DissapearEvents
-{
-    public DateTime EventsDate { get; set; }
-    public TimeSpan EventsHour { get; set; }
-        
-    public required string Circunstance { get; set; } //Rumbo al trabajo ...
-    public required string DetailedDescription { get; set; } //como, cuando y donde
-    public bool WasWithVictim { get; set; } //si / no
-        
-    //Georreferenciacion
-    public required string Street  { get; set; }
-    public required string Neighborhood  { get; set; }
-    public required string State { get; set; }
-    public required string Municipality  { get; set; }
-        
-    //Postgis y Neo4j
-    public double Latitude { get; set; }
-    public double Longitude { get; set; }
-} 
