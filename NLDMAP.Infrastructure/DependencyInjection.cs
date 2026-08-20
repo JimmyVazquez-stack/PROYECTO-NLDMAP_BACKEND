351beb4792bfbf5e9a17b9e44851e322704cb017
eduardo <eduardovasquez4545@hotmail.com>
2026-08-20 09:11:24 -0600

    agregar configuración para MongoDB y Neo4j en DependencyInjection

--- NLDMAP.Infrastructure/DependencyInjection.cs
+++ NLDMAP.Infrastructure/DependencyInjection.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
 using NLDMAP.Domain.Interfaces;
 using NLDMAP.Infrastructure.ExternalServices;
 using NLDMAP.Infrastructure.Persistence;
 using NLDMAP.Infrastructure.Repositories;
+using Neo4j.Driver;
+ 
 
 
 namespace NLDMAP.Infrastructure
@@ -16,11 +19,21 @@ namespace NLDMAP.Infrastructure
             services.AddPersistenceInfrastructure(configuration);
             
             //modulo 2 : Documentos y caracteristicas 
-            // services.AddMongoDbPersistence(configuration);
+            services.AddSingleton<IMongoClient>(sp =>
+            {
+                var connectionString = configuration.GetConnectionString("MongoConnection");
+                return new MongoClient(connectionString);
+            });
             
             //Modulo 3: Grafos
-            //services.AddNeo4jPersistence(configuration);
-            
+            services.AddSingleton<IDriver>(sp =>
+            {
+                var uri = configuration.GetConnectionString("Neo4jConnection");
+                var user = configuration["Neo4jSettings:User"];
+                var password = configuration["Neo4jSettings:Password"];
+
+                return GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+            });
             services.AddScoped<IUserRepository, EfUserRepository>();
             services.AddScoped<IExternalAuthValidator, GoogleAuthValidator>();
         
