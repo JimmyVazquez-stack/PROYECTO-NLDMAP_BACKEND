4d5da4efe848ca23d202d48553857cebefa6b6a9
eduardo <eduardovasquez4545@hotmail.com>
2026-08-20 15:55:32 -0600

    feat: agregar propiedades y constructores a la clase PhysicalCharacteristics

--- NLDMAP.Domain/ValueObjects/PhysicalCharacteristics.cs
+++ NLDMAP.Domain/ValueObjects/PhysicalCharacteristics.cs
@@ -5,7 +5,31 @@ namespace NLDMAP.Domain.ValueObjects
 
     public class PhysicalCharacteristics
     {
-        public 
+        public double Height { get; init; }
+        public string Build { get; init; }
+        public string HairColor { get; init; }
+        public string EyeColor { get; init; }
+        
+        //constructor para que el driver de mongodb no tenga conflictos al leer el json
+        internal PhysicalCharacteristics()
+        {
+            Build = string.Empty;
+            HairColor = string.Empty;
+            EyeColor = string.Empty;
+        }
+        
+        //Constructor publico donde exigimos datos para crear Value Objects
+        public PhysicalCharacteristics(double height, string build, string hairColor, string eyeColor)
+        {
+            if (height < 0 || height > 3.0)
+                throw new ArgumentException("La estatura ingresada no es valida");
+            
+            Height = height;
+            Build = build ?? string.Empty;
+            HairColor = hairColor ?? string.Empty;
+            EyeColor = eyeColor ?? string.Empty;
+        }
+        
     }
 
 }
