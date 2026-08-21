7229c36bb4f2a2e3e162676c98a158d8ec1a209f
eduardo <eduardovasquez4545@hotmail.com>
2026-08-21 12:09:53 -0600

    feat: agregar propiedades TituloReporte y UrlFoto a MapPointResponse

--- NLDMAP.Application/DTOs/Reports/MapPointResponse.cs
+++ NLDMAP.Application/DTOs/Reports/MapPointResponse.cs
@@ -5,6 +5,8 @@ namespace NLDMAP.Application.DTOs
         public Guid Id { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
-        public string Description { get; set; }
+        public string TituloReporte { get; set; } = string.Empty;
+        public string UrlFoto { get; set; } = string.Empty;
     }
+
 }
