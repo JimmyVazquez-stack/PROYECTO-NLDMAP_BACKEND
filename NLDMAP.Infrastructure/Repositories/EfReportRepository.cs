80c908f4dd113b1d808425a0c4360b53b8d9567a
eduardo <eduardovasquez4545@hotmail.com>
2026-08-24 09:13:30 -0600

    refactor: remover async/await de metodos EfReportRepository

--- NLDMAP.Infrastructure/Repositories/EfReportRepository.cs
+++ NLDMAP.Infrastructure/Repositories/EfReportRepository.cs
@@ -8,27 +8,27 @@ namespace NLDMAP.Infrastructure.Repositories
 
     public class EfReportRepository : IReportRepository
     {
-        public async Task AddAsync(Report report)
+        public  Task AddAsync(Report report)
         {
             //Implementacion en Postgresql, mongodb y neo4j
-            await Task.CompletedTask;
+            return Task.CompletedTask;
         }
 
         public Task<Report> GetByFolioAsync(string folio)
         {
             //implementar busqueda real 
-            return await Task.FromResult<Report>(null!);
+            return  Task.FromResult<Report>(null!);
         }
 
-        public async Task<Report> GetByIdAsync(Guid id)
+        public Task<Report> GetByIdAsync(Guid id)
         {
-            return await Task.FromResult<Report>(null!);
+            return  Task.FromResult<Report>(null!);
         }
 
         public Task UpdateAsync(Report report)
         {
             //implementar actualizacion real
-            await Task.CompletedTask;
+            return Task.CompletedTask;
         }
     }
 }
